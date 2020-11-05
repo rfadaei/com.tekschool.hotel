41effbafcfc896533a54f4c1467b3d77cc29d08c
tekschool <alirezakhamosh99@gmail.com>
2020-11-04 21:21:02 -0500

    new class have been added

---
+++ src/test/java/testNG/concepts/TestClass.java
@@ -0,0 +1,13 @@
+package testNG.concepts;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	
+	@Test
+	public void test() {
+		System.out.println("this class will be pushed to repo");
+	}
+
+}
