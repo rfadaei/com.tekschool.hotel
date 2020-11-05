3acfb609738e8846a25609a5cdecb3e4b4bac07c
tekschool <73264172+tekschool@users.noreply.github.com>
2020-11-04 21:25:56 -0500

    Update TestClass.java
    changes in git hub

--- src/test/java/testNG/concepts/TestClass.java
+++ src/test/java/testNG/concepts/TestClass.java
@@ -8,6 +8,9 @@ public class TestClass {
 	@Test
 	public void test() {
 		System.out.println("this class will be pushed to repo");
+		// this is comments from Git Hub account 
+		// These are changes made in Git Repo 
+		// to pull these changes we need to fitch from upstream or use git pull command
 	}
 
 }
